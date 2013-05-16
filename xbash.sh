b53c0517f152255647a88f71a9ed101699a08950
Eduardo Cuomo <eduardo.cuomo.ar@gmail.com>
2013-05-16 09:06:18 -0300

    Update xbash.sh

--- xbash.sh
+++ xbash.sh
@@ -145,14 +145,14 @@ fi
     
     # Sub string.
     # 1: {String} String to cut.
-    # 2: {Integer} Start.
-    # 3: {Integer} (Optional) Count.
+    # 2: {Integer} Limit.
+    # 3: {Integer} (Optional) Offset.
     # Out: {String} Result string
     function sub_str() {
         if [ $# -eq 2 ] ; then
-            echo ${1:$2}
+            echo ${1:0:$2}
         else
-            echo ${1:$2:$3}
+            echo ${1:$3:$2}
         fi
     }
 
