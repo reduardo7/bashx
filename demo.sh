b3b35bd4034d756ba29da254bf4edf051a0c780e
Eduardo Cuomo <eduardo.cuomo.ar@gmail.com>
2013-05-17 21:18:48 -0300

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -3,22 +3,27 @@
 
 APP_TITLE="Demo XBash"
 APP_VERSION="0.1"
+DEFAULT_COLOR="cyan2"
 
-function info() { #ARG: See info
+function __info() { #%nSee info
     # Info
-    e "Script name: $(script_file_name)"
-    e "Path: $(current_directory_name)"
+    e "$(ecolor green)Script name: $(script_file_name)"
+    e "$(ecolor red)Path: $(current_directory_name)"
     e "Date and time: $(now_date_time)"
-    
-    # Separator
-    e
+        
+    # Pause script
+    pause
     
     # Check root
     if [ $(is_root) = $TRUE ] ; then
-        e "ROOT user"
+        e "$(ecolor red)ROOT$(ecolor green) user"
     else
-        e "No ROOT user"
+        e "$(ecolor green)No $(ecolor red)ROOT$(ecolor green) user"
     fi
+}
+
+function __params() { # param1 %nparam1: Text to print.
+    e "Text passed: $(ecolor red) '$1'"
 }
 
 # Run
