0de46f1bef64ed831d0e558af73b8aea9441b4a8
Eduardo Cuomo <eduardo.cuomo@patagonian.it>
2015-10-25 20:05:30 -0300

    Add run.sh

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# ################### #
+#                     #
+# Application Manager #
+#                     #
+# ################### #
+
+# Load
+. "$(dirname "$0")/src/bashx.sh"
+# Run
+run "$@"
