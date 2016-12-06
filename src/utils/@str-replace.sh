9d5689976417645b7ac91766bf412887d21df927
Eduardo Cuomo <eduardo.cuomo@patagonian.it>
2016-12-06 10:40:23 -0300

    MacOS Fixes: sed ignore case

--- src/utils/@str-replace.sh
+++ src/utils/@str-replace.sh
@@ -7,6 +7,10 @@
 # Out: {String} Result string.
 local options="g"
 if [ $# -lt 4 ] || [ "$4" -ne $TRUE ]; then
-  options="${options}i"
+  if $OS_IS_MAC; then
+    options="${options}I"
+  else
+    options="${options}i"
+  fi
 fi
 echo "$1" | sed "s/$2/$3/$options"
