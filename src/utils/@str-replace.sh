ecb3a1aa718862bb476eb0807224e123fa75e3d1
Eduardo Cuomo <eduardo.cuomo@patagonian.it>
2016-12-06 10:47:26 -0300

    MacOS Fixes: sed ignore case

--- src/utils/@str-replace.sh
+++ src/utils/@str-replace.sh
@@ -5,12 +5,16 @@
 # 3: {String} Replace.
 # 4: {Boolean} (Default: 1) 1 to ignore case.
 # Out: {String} Result string.
+
 local options="g"
 if [ $# -lt 4 ] || [ "$4" -ne $TRUE ]; then
-  if $OS_IS_MAC; then
-    options="${options}I"
-  else
-    options="${options}i"
-  fi
+  options="${options}i"
 fi
-echo "$1" | sed "s/$2/$3/$options"
+
+local reg="s/$2/$3/$options"
+
+if $OS_IS_MAC; then
+  perl -C -Mutf8 -pe "$reg" <<< "$1"
+else
+  sed "$reg" <<< "$1"
+fi
