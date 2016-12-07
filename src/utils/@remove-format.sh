2390c3fd5dff38a48b7a9feb14a18b0aea9d2f5f
Eduardo Cuomo <eduardo.cuomo@patagonian.it>
2016-12-07 10:46:49 -0300

    MacOS Fixes: sed regexp

--- src/utils/@remove-format.sh
+++ src/utils/@remove-format.sh
@@ -5,4 +5,8 @@
 
 # http://www.commandlinefu.com/commands/view/3584/remove-color-codes-special-characters-with-sed
 
-echo "$@" | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g"
+local pr
+
+$OS_IS_MAC && pr='E' || pr='r'
+
+echo "$@" | sed -${pr} "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g"
