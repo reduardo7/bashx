29b41dfb6d17ec064ca51fb833a4348ec98bc991
Eduardo Cuomo <eduardo.cuomo@patagonian.it>
2016-12-13 12:21:10 -0300

    @e

--- src/utils/@e.sh
+++ src/utils/@e.sh
@@ -4,16 +4,9 @@
 # Out: {String} Text.
 
 local str="$@"
-local reg='s/\t/    /g'
 
 # \t -> \s*4
-if $OS_IS_MAC; then
-  str="$(perl -C -Mutf8 -pe "${reg}" <<< "${str}")"
-else
-  str=$(sed "$reg" <<< "${str}")
-fi
-
-echo -e "$(@style default)${ECHO_CHAR} ${str}" >&2
+echo -e "$(@style default)${ECHO_CHAR} $(@str-replace "${str}" '\t' '    ')" >&2
 
 # Style reset for next command
 @style reset >&2
