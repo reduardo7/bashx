240ced0a14da8c60c53c4b34355c6d3b6b615526
Eduardo Cuomo <reduardo7@gmail.com>
2018-03-20 18:09:26 -0300

    install-bin fix

--- src/actions/install-bin.sh
+++ src/actions/install-bin.sh
@@ -20,103 +20,105 @@ local p
 local r
 
 if [ "$force" == "-f" ]; then
-	force=true
+  force=true
 else
-	force=false
+  force=false
 fi
 
 case "$action" in
-	"install")
-		if $force || [ ! -f "$bcfile" ]; then
-			# Create file
-			e "Creating '`@style bold`$bcfile`@style default`' file..."
+  "install")
+    if $force || [ ! -f "$bcfile" ]; then
+      # Create file
+      @print "Creating '`@style bold`$bcfile`@style default`' file..."
 
-			echo "# bash completion for $scrpt (`@script-full-path`)" > "$bcfile"
-			echo "_${scrpt}_methods() {" >> "$bcfile"
-			echo "	grep \"^\\s*\\(function\\s\\+\\)\\?${_ACTION_PREFIX}.\\+()\\s*{.*\$\" \"\${1}\" | while read line ; do" >> "$bcfile"
-			echo "		echo \"\$line\" | sed \"s/()\\s*{.*//g\" | sed \"s/\\s*\\(function\\s\\+\\)\\?${_ACTION_PREFIX}//g\"" >> "$bcfile"
-			echo "	done" >> "$bcfile"
-			echo "}" >> "$bcfile"
-			echo "_${scrpt}_lst() {" >> "$bcfile"
-			echo "	if [ -d \"${ACTIONS_PATH}\" ]; then" >> "$bcfile"
-			echo "		for f in ${ACTIONS_PATH}/* ; do" >> "$bcfile"
-			echo "			if [ -f \"\${f}\" ]; then" >> "$bcfile"
-			echo "				basename \"\${f}\" | sed 's/\..*\$//g'" >> "$bcfile"
-			echo "			fi" >> "$bcfile"
-			echo "		done" >> "$bcfile"
-			echo "	fi" >> "$bcfile"
-			echo "	_${scrpt}_methods \"`@script-full-path`\"" >> "$bcfile"
-			echo "	_${scrpt}_methods \"${BASHX_BASE_SOURCE}\"" >> "$bcfile"
-			echo "}" >> "$bcfile"
-			echo "_${scrpt}() {" >> "$bcfile"
-			echo "	local cur" >> "$bcfile"
-			echo "	COMPREPLY=()" >> "$bcfile"
-			echo "	cur=\${COMP_WORDS[COMP_CWORD]}" >> "$bcfile"
-			echo "	COMPREPLY=( \$( compgen -W '\$( _${scrpt}_lst )' -- \$cur  ) )" >> "$bcfile"
-			echo "}" >> "$bcfile"
-			echo "_${scrpt}_zsh() {" >> "$bcfile"
-			echo "	compadd \`_${scrpt}_lst\`" >> "$bcfile"
-			echo "}" >> "$bcfile"
-			echo "if type complete >/dev/null 2>/dev/null; then" >> "$bcfile"
-			echo "	# bash" >> "$bcfile"
-			echo "	complete -F _${scrpt} ${scrpt}" >> "$bcfile"
-			echo "else if type compdef >/dev/null 2>/dev/null; then" >> "$bcfile"
-			echo "	# zsh" >> "$bcfile"
-			echo "	compdef _${scrpt}_zsh ${scrpt}" >> "$bcfile"
-			echo "fi; fi" >> "$bcfile"
-		fi
+cat > "$bcfile" <<EOF
+# bash completion for $scrpt (`@script-full-path`)
+_${scrpt}_methods() {
+  grep \"^\\s*\\(function\\s\\+\\)\\?${_ACTION_PREFIX}.\\+()\\s*{.*\$\" \"\${1}\" | while read line ; do
+    echo \"\$line\" | sed \"s/()\\s*{.*//g\" | sed \"s/\\s*\\(function\\s\\+\\)\\?${_ACTION_PREFIX}//g\"
+  done
+}
+_${scrpt}_lst() {
+  if [ -d \"${ACTIONS_PATH}\" ]; then
+    for f in ${ACTIONS_PATH}/* ; do
+      if [ -f \"\${f}\" ]; then
+        basename \"\${f}\" | sed 's/\..*\$//g'
+      fi
+    done
+  fi
+  _${scrpt}_methods \"`@script-full-path`\"
+  _${scrpt}_methods \"${BASHX_BASE_SOURCE}\"
+}
+_${scrpt}() {
+  local cur
+  COMPREPLY=()
+  cur=\${COMP_WORDS[COMP_CWORD]}
+  COMPREPLY=( \$( compgen -W '\$( _${scrpt}_lst )' -- \$cur  ) )
+}
+_${scrpt}_zsh() {
+  compadd \`_${scrpt}_lst\`
+}
+if type complete >/dev/null 2>/dev/null; then
+  # bash
+  complete -F _${scrpt} ${scrpt}
+else if type compdef >/dev/null 2>/dev/null; then
+  # zsh
+  compdef _${scrpt}_zsh ${scrpt}
+fi; fi
+EOF
+    fi
 
-		for r in $rcs ; do
-			p="$HOME/$r"
-			if [ -f "$p" ]; then
-				if grep "$l" "$p" >/dev/null 2>&1
-					then
-						e "Already installed in '`@style bold`$r`@style default`'"
-					else
-						e "Installing in '`@style bold`$r`@style default`'..."
-						echo >> "$p"
-						echo "$l" >> "$p"
-					fi
-			fi
-		done
+    for r in $rcs ; do
+      p="$HOME/$r"
+      if [ -f "$p" ]; then
+        if grep "$l" "$p" >/dev/null 2>&1
+          then
+            @print "Already installed in '`@style bold`$r`@style default`'"
+          else
+            @print "Installing in '`@style bold`$r`@style default`'..."
+            echo >> "$p"
+            echo "$l" >> "$p"
+          fi
+      fi
+    done
 
-		e "Done!"
-	;;
-	"uninstall")
-		e "Uninstalling..."
-		if [ -f "$bcfile" ]; then
-			# Delete file
-			e "Deleting '`@style bold`$bcfile`@style default`' file..."
-			if $force; then
-				# Force
-				rm -f "$bcfile"
-			else
-				# Interactive
-				rm -i "$bcfile"
-			fi
-		fi
+    @print "Done!"
+  ;;
+  "uninstall")
+    @print "Uninstalling..."
+    if [ -f "$bcfile" ]; then
+      # Delete file
+      @print "Deleting '`@style bold`$bcfile`@style default`' file..."
+      if $force; then
+        # Force
+        rm -f "$bcfile"
+      else
+        # Interactive
+        rm -i "$bcfile"
+      fi
+    fi
 
-		for r in $rcs ; do
-			p="$HOME/$r"
-			if [ -f "$p" ]; then
-				if grep "$l" "$p" >/dev/null 2>&1
-					then
-						e "Removing from '`@style bold`$r`@style default`'..."
-						if $force; then
-							# Force
-							cat "$p" | grep -v "$l" > "${p}.2" && mv -f "${p}.2" "$p"
-						else
-							# Interactive
-							cat "$p" | grep -v "$l" > "${p}.2" && mv -i "${p}.2" "$p"
-						fi
-					fi
-			fi
-		done
+    for r in $rcs ; do
+      p="$HOME/$r"
+      if [ -f "$p" ]; then
+        if grep "$l" "$p" >/dev/null 2>&1
+          then
+            @print "Removing from '`@style bold`$r`@style default`'..."
+            if $force; then
+              # Force
+              cat "$p" | grep -v "$l" > "${p}.2" && mv -f "${p}.2" "$p"
+            else
+              # Interactive
+              cat "$p" | grep -v "$l" > "${p}.2" && mv -i "${p}.2" "$p"
+            fi
+          fi
+      fi
+    done
 
-		e "Done!"
-	;;
-	*)
-		# Invalid action
-		error "Invalid action '`@style bold`$action`@style default`'"
-	;;
+    @print "Done!"
+  ;;
+  *)
+    # Invalid action
+    @error "Invalid action '`@style bold`$action`@style default`'"
+  ;;
 esac
