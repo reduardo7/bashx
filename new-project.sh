48c1dbb32d4ce17a110021062c20e43b5bba5c06
Eduardo Cuomo <reduardo7@gmail.com>
2018-03-22 15:49:56 -0300

    new-project.sh examlpe.

--- new-project.sh
+++ new-project.sh
@@ -4,6 +4,7 @@ set -e
 
 BASHX_VERSION="$1"
 PROJECT_NAME="$2"
+_ACTION_PREFIX="@Actions"
 
 _log() {
   echo "# $*"
@@ -30,22 +31,35 @@ _log Writting file ${PROJECT_NAME}...
 
 touch "${PROJECT_NAME}"
 
-echo '#!/usr/bin/env bash' >"${PROJECT_NAME}"
-echo >>"${PROJECT_NAME}"
-echo '# BashX | https://github.com/reduardo7/bashx' >>"${PROJECT_NAME}"
-echo "export BASHX_VERSION=\"${BASHX_VERSION}\" ; bash -c \"\$(echo '$(echo "${code}" | base64)' | base64 -d)\" || exit \$?" >>"${PROJECT_NAME}"
-echo ". \"\${HOME}/.bashx/\${BASHX_VERSION}/init\"" >>"${PROJECT_NAME}"
-echo >>"${PROJECT_NAME}"
-echo >>"${PROJECT_NAME}"
-echo '# ... Your code here ...' >>"${PROJECT_NAME}"
-echo >>"${PROJECT_NAME}"
-echo >>"${PROJECT_NAME}"
-echo '@init "$@"' >>"${PROJECT_NAME}"
-echo >>"${PROJECT_NAME}"
+cat > "${PROJECT_NAME}" <<EOF
+#!/usr/bin/env bash
+
+# BashX | https://github.com/reduardo7/bashx
+export BASHX_VERSION="${BASHX_VERSION}"
+bash -c "\$(echo '$(echo "${code}" | base64)' | base64 -d)" || exit \$?
+. "\${HOME}/.bashx/\${BASHX_VERSION}/init"
+
+### Begin Example ###
+
+${_ACTION_PREFIX}.action1() { # \\\\n Action without arguments
+  # ... Your code here ...
+  @print Action 1
+}
+
+${_ACTION_PREFIX}.action2() { # param1 param2 \\\\n Action with arguments
+  # ... Your code here ...
+  @print Action 2
+  @print Param1: \$1
+  @print Param2: \$2
+}
+
+### End Example ###
+
+@init "\$@"
+EOF
 
 chmod a+x "${PROJECT_NAME}"
 
 ###############################################################################
 
 _log Project created at ${PROJECT_NAME}
-
