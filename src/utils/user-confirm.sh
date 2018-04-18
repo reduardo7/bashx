49ff629d0b0631e429a9c4d9f41d961939fbf58b
Eduardo Cuomo <reduardo7@gmail.com>
2018-04-18 20:13:51 -0300

    @user-confirm reimplementation

--- src/utils/user-confirm.sh
+++ src/utils/user-confirm.sh
@@ -1,42 +1,21 @@
-## [msg [options [default]]]
+## [msg]
 ## User confirm.
 ##
 ## msg:     {String} (Optional) Message.
-## options: {Array} (Default: ( "y" )) Valid options. Case insensitive.
-## default: {Boolean} (Default: false) Default result on non user input.
-##          true to confirm, false to no confirm.
 ## Return:  0 if user confirm, 1 if user not confirm.
 
-local m="$1"
-local o=( $2 )
-local d=$3
+local msg="$1"
 
-[ -z "${m}" ] && m='Confirm? [Y = Yes | N = No]'
-[ -z "${o}" ] && o=( 'y' )
-
-if [ -z "${d}" ]; then
-  d=1
-else
-  ${d} && d=0 || d=1
+if [ -z "$msg" ]; then
+  msg="Confirm?"
 fi
 
-# Read
-local i=''
-read -n 1 -s -p "$(@style default)${APP_PRINT_PREFIX} ${m}: " i >&3
-echo >&3
+echo -n "$(@style default)${APP_PRINT_PREFIX} $msg (y/n) " >&3
 
-i="$(@trim "${i}")"
-
-if [ -z "${i}" ]; then
-  return ${d}
-else
-  # Validate input
-  for x in $o; do
-    if [ "$(@str-to-lower "${x}")" == "$(@str-to-lower "${i}")" ]; then
-      # User accept
-      return 0
-    fi
-  done
-fi
-
-return 1
+while true ; do
+  read -s -n 1 choice >&3
+  case "$choice" in
+    y|Y ) echo Y >&3 ; return 0 ;;
+    n|N ) echo N >&3 ; return 1 ;;
+  esac
+done
