f8c52386f14a7c2d861cdc9b50b7d04cab0a6ade
Eduardo Cuomo <reduardo7@gmail.com>
2018-05-03 17:47:54 -0300

    online setup script

--- src/init-app.sh
+++ src/init-app.sh
@@ -15,40 +15,24 @@
 export LC_CTYPE=C
 export LC_ALL=C
 
-_e(){ echo "# $*";}
-_w(){ _e $* >&2;}
-_x(){ _w Error: $*;exit 1;}
+set -e
 
-[ -z "${BASHX_VERSION}" ] && _x BASHX_VERSION is required
+_x() {
+  echo "# Error: ${1:-Installation fail}" >&2
+  exit 1
+}
+
+[ -z "${BASHX_VERSION}" ] && _x 'BASHX_VERSION is required'
 
 export BASHX_DIR="${BASHX_DIR:-$HOME/.bashx/$BASHX_VERSION}"
-export t="$(mktemp -d)"
 
-# Install
-(
-set -e
 if [ ! -d "$BASHX_DIR" ]; then
-  _e Installing BashX ${BASHX_VERSION}...
-
-  m="Error downloading BashX ${BASHX_VERSION}"
-  g="https://github.com/reduardo7/bashx/tarball/${BASHX_VERSION}"
-
-  cd "$t"
-
+  export setup_url='https://raw.githubusercontent.com/reduardo7/bashx/master/src/setup.sh'
   if type wget >/dev/null 2>&1 ; then
-    wget --no-check-certificate $g -O - | tar -xz || _x $m
+    sh -c "$(wget $setup_url -O -)" || _x
   elif type curl >/dev/null 2>&1 ; then
-    curl -sL $g | tar -xz || _x $m
+    sh -c "$(curl -fsSL $setup_url)" || _x
   else
-    _x wget or curl are required. Install wget or curl to continue
+    _x 'wget or curl are required. Install wget or curl to continue'
   fi
-
-  mkdir -p "$BASHX_DIR"
-  rm -rf "$BASHX_DIR"
-  mv reduardo7-bashx-* "$BASHX_DIR"
 fi
-)
-e=$?
-# Cleanup
-rm -rf "$t" >/dev/null 2>&1 || true
-exit $e
---
+++ src/setup.sh
@@ -0,0 +1,63 @@
+############################################
+#                                          #
+# BashX                                    #
+#                                          #
+# Extended Bash Framework.                 #
+#                                          #
+# URL: https://github.com/reduardo7/bashx  #
+#                                          #
+# Author: Eduardo Daniel Cuomo             #
+#         eduardo.cuomo.ar@gmail.com       #
+#         reduardo7@gmail.com              #
+#                                          #
+############################################
+
+export LC_CTYPE=C
+export LC_ALL=C
+
+_e() {
+  echo "# $*"
+}
+
+_x() {
+  _e "Error: $*" >&2
+  exit 1
+}
+
+[ -z "${BASHX_VERSION}" ] && _x BASHX_VERSION is required
+type wget >/dev/null 2>&1 || _x tar is required. Install tar to continue
+export BASHX_DIR="${BASHX_DIR:-$HOME/.bashx/$BASHX_VERSION}"
+
+if [ ! -d "$BASHX_DIR" ]; then
+  export t="$(mktemp -d)"
+
+  # Install
+  (
+    set -e
+    _e Installing BashX ${BASHX_VERSION}...
+
+    m="Error downloading BashX ${BASHX_VERSION}"
+    g="https://github.com/reduardo7/bashx/tarball/${BASHX_VERSION}"
+
+    cd "$t"
+
+    if type wget >/dev/null 2>&1 ; then
+      wget --no-check-certificate $g -O - | tar -xz || _x $m
+    elif type curl >/dev/null 2>&1 ; then
+      curl -sL $g | tar -xz || _x $m
+    else
+      _x wget or curl are required. Install wget or curl to continue
+    fi
+
+    mkdir -p "$BASHX_DIR"
+    rm -rf "$BASHX_DIR"
+    mv reduardo7-bashx-* "$BASHX_DIR"
+  )
+  e=$?
+
+  # Cleanup
+  rm -rf "$t" >/dev/null 2>&1 || true
+
+  # Finish
+  exit $e
+fi
