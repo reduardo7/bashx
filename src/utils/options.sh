c2b1f47f699fc59542977abb6451a5465c31f1d6
Eduardo Cuomo <reduardo7@gmail.com>
2018-06-21 18:32:10 -0300

    WIP: @options

--- src/utils/options.sh
+++ src/utils/options.sh
@@ -1,8 +1,8 @@
-## (Options*)
+## (options*)
 ## Read options.
 ##
 ## Params:
-##   (Options*): {Map} Valid options specifications.
+##   (options*): {Map} Valid options specifications.
 ##
 ##
 ## Options Format: ( KEY:VARIABLE:REQUIRED:INPUT ... )
@@ -21,6 +21,31 @@
 ##     - OPTARG_PATH with user input value, or EMPTY if no
 ##
 ## Usage example: eval $(@options ( n:new p:path:false ))
+
+local options="$@"
+local OIFS="$IFS"
+local variable
+local config
+local config_key
+local config_var
+local config_required
+local config_input
+local config_var_prefix='OPTARG_'
+
+IFS=' ' options=(${options}) IFS="$OIFS"
+
+for variable in ${variables[@]} ; do
+  IFS=':' config=(${variable}) IFS="$OIFS"
+  config_key="${config[0]}"
+  config_var="${config[1]}"
+  config_required=${config[2]:-true}
+  config_input=${config[3]:-false}
+
+  [ ! -z "${config_var}" ] || @throw-invalid-param options
+  config_var="${config_var_prefix}$(@str-to-upper "${config_var}")"
+done
+
+
 
 OPTARG_NET=false
 OPTARG_PORTS='' #@TODO
