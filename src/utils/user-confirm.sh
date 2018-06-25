27f6a3d721e3268316a16dfd5ae0315abf2cc1c6
Eduardo Cuomo <reduardo7@gmail.com>
2018-06-25 15:05:26 -0300

    @user-confirm delay

--- src/utils/user-confirm.sh
+++ src/utils/user-confirm.sh
@@ -8,14 +8,25 @@
 ## Return: 0 if user confirm, 1 if user not confirm.
 
 local msg="${1:-Confirm?}"
+local choice
+local result=3
 
 echo -n "$(@style default)${BX_APP_PRINT_PREFIX} ${msg} (y/n) " >&3
 
-while true ; do
+while [[ ${result} -gt 1 ]] ; do
   read -s -n 1 choice >&3
 
-  case "$choice" in
-    y|Y ) echo Y >&3 ; return 0 ;;
-    n|N ) echo N >&3 ; return 1 ;;
+  case "${choice}" in
+    y|Y ) result=0 ;;
+    n|N ) result=1 ;;
   esac
 done
+
+if [[ ${result} -eq 0 ]] ; then
+  echo Y >&3
+else
+  echo N >&3
+fi
+
+read -s -t 0.5 choice
+return ${result}
