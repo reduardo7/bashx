fb8a0cf7ffee2a261e7eeb7f75f34362e1af1ff5
Eduardo Cuomo <reduardo7@gmail.com>
2019-03-30 17:13:36 -0300

    v1.7.0

--- src/actions/_bashx.sh
+++ src/actions/_bashx.sh
@@ -45,7 +45,7 @@
 ##   * Remove "bar" from Utils:
 ##       _bashx util remove bar
 ##   * Initialize project:
-##       _bashx init project v1.6.3 ~/project/my-script 'My Super-Script'
+##       _bashx init project v1.7.0 ~/project/my-script 'My Super-Script'
 
 @title "$(@style bold color:red)BashX$(@style default) Framework Utils"
 
