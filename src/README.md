ca7d0df8e73c5651299fb731d7e35b33fb7dc89c
Eduardo Cuomo <reduardo7@gmail.com>
2022-01-30 16:00:32 -0300

    Configurations & Constants files

--- src/README.md
+++ src/README.md
@@ -2,22 +2,56 @@
 
 ## [`/bootstrap.src`](bootstrap.src)
 
-Script to initialize the _main APP_.
+Uncompressed script to initialize the _main APP_.
 
 It's present at the beginning of _BashX_ entry-point script.
 
+## [`/config.init.src`](config.init.src)
+
+Initial _BashX_ project configuration.
+Should be override before load the _bootstrap_ script,
+or before call the entrypoint script.
+
+## [`/config.src`](config.src)
+
+_BashX_ project configuration.
+Can be override by custom project configuration.
+
+## [`/constants.src`](constants.src)
+
+Readonly _BashX_ internal constants.
+
+## [`/init.sh`](init.sh)
+
+Initial _BashX_ script.
+In charge of loading everything before start.
+
 ## [`/setup.sh`](setup.sh)
 
-Script to install _BashX_.
+First time install _BashX_ script.
 
 ## [`/actions`](actions)
 
-Available _BashX_ **actions**.
+Available _BashX_ **actions** script.
 
 ## [`/tests`](tests)
 
-Available _BashX_ **tests**.
+Available _BashX_ **tests** scripts.
 
 ## [`/utils`](utils)
 
-Available _BashX_ **utils**.
+Available _BashX_ **utils** scripts.
+You can call each one using the prefix `@` and the filename.
+
+_For example:_
+
+```bash
+# src/utils/myScript.xsh
+@myScript
+
+# src/utils/myScript/foo.xsh
+@myScript.foo
+
+# src/utils/helpers/bar.xsh
+@helpers.bar
+```
--- src/init.sh
+++ src/init.sh
@@ -36,6 +36,13 @@ if [ -z "${BASH}" ]; then
   exit $?
 fi
 
+## `BASHX_VERSION` constant should be defined before load the bootstrap.
+readonly BASHX_VERSION="${BASHX_VERSION}"
+[ ! -z "${BASHX_VERSION}" ] || {
+  echo '"BASHX_VERSION" is required!' >&2
+  exit 1
+}
+
 # #############################################################################
 
 ### Ensure environment constants
@@ -43,150 +50,19 @@ fi
 [[ ! -z "${UID}" ]] || export UID=$(id -u)
 [[ ! -z "${GID}" ]] || export GID=$(id -g)
 
-### Constants
-
-## BashX called script arguments.
-export BX_SCRIPT_ARGS=($@)
-
-## BashX scripts extension.
-readonly BX_SCRIPT_EXTENSION='xsh'
-
-## Called script.
-readonly BX_SCRIPT_CALLED="${BX_SCRIPT_CALLED}"
-
-## Current script file name.
-readonly BX_SCRIPT_FILE_NAME="${BX_SCRIPT_FILE_NAME}"
-
-## Current script full path.
-readonly BX_SCRIPT_FULL_PATH="${BX_SCRIPT_DIR}/${BX_SCRIPT_FILE_NAME}"
-
-## Current script directory.
-readonly BX_SCRIPT_DIR="${BX_SCRIPT_DIR}"
-
 ## Core directory (can be overridden before load with BX_DIR).
 readonly BX_DIR="${BX_DIR:-${HOME:-/tmp}/.bashx/${BASHX_VERSION}}"
+[ -d "${BX_DIR}" ] || {
+  echo "[BX_DIR=${BX_DIR}] is invelid!" >&2
+  exit 2
+}
 
 ## Core SRC path.
 readonly BX_SRC_PATH="${BX_DIR}/src"
 
-## Core actions path.
-readonly BX_ACTIONS_PATH="${BX_SRC_PATH}/actions"
-
-## Core utils path.
-readonly BX_UTILS_PATH="${BX_SRC_PATH}/utils"
-
-### OS Constants
-
-export BX_OS_IS_LINUX=false
-export BX_OS_IS_MAC=false
-export BX_OS_IS_MINGW=false
-
-if [[ "$(uname)" == "Darwin" ]]; then
-  defaults write org.R-project.R force.LANG en_US.UTF-8
-  export TERM="xterm-color"
-  export BX_OS_IS_MAC=true
-elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
-  export BX_OS_IS_LINUX=true
-elif [[ "$(expr substr $(uname -s) 1 10)" == MINGW* ]]; then
-  export BX_OS_IS_MINGW=true
-fi
-
-## Is Linux OS?
-readonly BX_OS_IS_LINUX=${BX_OS_IS_LINUX}
-## Is Mac OS?
-readonly BX_OS_IS_MAC=${BX_OS_IS_MAC}
-## Is Win OS?
-readonly BX_OS_IS_MINGW=${BX_OS_IS_MINGW}
-
-BX_BASE_SOURCE="${BASH_SOURCE[0]}"
-while [ -h "${BX_BASE_SOURCE}" ]; do
-  BX_BASE_DIR="$(cd -P "$(dirname "${BX_BASE_SOURCE}")" && pwd)"
-  BX_BASE_SOURCE=`readlink "${BX_BASE_SOURCE}"`
-  [[ "${BX_BASE_SOURCE}" != /* ]] && BX_BASE_SOURCE="${BX_BASE_DIR}/${BX_BASE_SOURCE}"
-done
-[ -z "${BX_BASE_DIR}"] && BX_BASE_DIR="$(cd -P "$(dirname "${BX_BASE_SOURCE}")" && pwd)"
-
-## BashX base source path.
-readonly BX_BASE_SOURCE="${BX_BASE_SOURCE}"
-
-# BashX base directory path.
-readonly BX_BASE_DIR="${BX_BASE_DIR}"
-
-# \e | \033 | \x1B
-## Key: ESC
-${BX_OS_IS_MAC} && readonly BX_KEY_ESC=$'\x1B' || readonly BX_KEY_ESC=$'\e'
-
-# https://stackoverflow.com/a/911213/717267
-## TTY available?
-[ -t 1 ] && readonly BX_TTY=true || readonly BX_TTY=false
-
-# \n - New Line
-## New Line constant (\n)
-readonly BX_CHAR_NL='
-'
-
-# \t - Tab
-## New Line constant (\t)
-readonly BX_CHAR_TAB='  '
-
-## Action prefix. Used at @app.run.
-readonly BX_ACTION_PREFIX='@Actions'
-
-## Events options.
-readonly BX_EVENTS_OPTS=(invalid-action ready start error finish)
-
-# #############################################################################
-
-### Configs
-
-export BASHX_DOC_MARK='##'
-
-## Current script Sources directory. Customizable with: BASHX_SRC_DIR.
-export BASHX_SRC_DIR="${BASHX_SRC_DIR:-${BX_SCRIPT_FILE_NAME}.src}"
-
-## Current script Sources path. Constant.
-export BASHX_SRC_PATH="${BX_SCRIPT_DIR}/${BASHX_SRC_DIR}"
-
-## Configuration file (can be overridden before load). Customizable with: BASHX_APP_CONFIG_FILE.
-export BASHX_APP_CONFIG_FILE="${BASHX_APP_CONFIG_FILE:-${BX_SCRIPT_DIR}/.env}"
-
-## APP Title. Customizable with: BASHX_APP_CONFIG_FILE.
-export BASHX_APP_TITLE="${BASHX_APP_TITLE:-BashX}"
-
-## APP Version.
-export BASHX_APP_VERSION="${BASHX_APP_VERSION:-1.0}"
-
-## Default APP color. See "@style" for more information.
-export BASHX_APP_COLOR_DEFAULT="${BASHX_APP_COLOR_DEFAULT:-cyan-light}"
-
-## Output colors?
-export BASHX_APP_COLORS_ENABLED=${BASHX_APP_COLORS_ENABLED:-true}
-
-## APP colsumns width.
-export BASHX_APP_WIDTH=${BASHX_APP_WIDTH:-80}
-
-## Start character for formated print screen. See "@log" for more information.
-export BASHX_APP_PRINT_PREFIX="${BASHX_APP_PRINT_PREFIX:-#}"
-
-## Default action to call. Action to use if script called without arguments.
-export BASHX_APP_DEFAULT_ACTION="${BASHX_APP_DEFAULT_ACTION:-help}"
-
-## Tests path.
-export BASHX_TESTS_PATH="${BASHX_SRC_PATH}/tests"
-
-## Actions path.
-export BASHX_ACTIONS_PATH="${BASHX_SRC_PATH}/actions"
-
-## Utils path.
-export BASHX_UTILS_PATH="${BASHX_SRC_PATH}/utils"
-
-## Events path.
-export BASHX_EVENTS_PATH="${BASHX_SRC_PATH}/events"
-
-## Resources path.
-export BASHX_RESOURCES_PATH="${BASHX_SRC_PATH}/resources"
-
-# #############################################################################
+. "${BX_SRC_PATH}/config.init.src"
+. "${BX_SRC_PATH}/constants.src"
+. "${BX_SRC_PATH}/config.src"
 
 ### Load config
 
